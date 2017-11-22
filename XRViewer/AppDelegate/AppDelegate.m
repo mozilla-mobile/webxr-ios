b5d6ed25ebb1258d036710b398ae68e647a16af9
Roberto Garrido <robertogarridomartin@gmail.com>
2017-11-22 16:19:16 +0100

    the app can be opened now via external URL. If you open with a browser a link like: webxr://ios-viewer.webxrexperiments.com/viewer.html, the loads https://ios-viewer.webxrexperiments.com/viewer.html

--- XRViewer/AppDelegate/AppDelegate.m
+++ XRViewer/AppDelegate/AppDelegate.m
@@ -12,4 +12,20 @@
     return YES;
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    //Be ready to open URLs like "webxr://ios-viewer.webxrexperiments.com/viewer.html"
+    if ([[url scheme] isEqualToString:@"webxr"]) {
+        // Extract the scheme part of the URL
+        NSString* urlString = [[url absoluteString] substringFromIndex:8];
+        urlString = [NSString stringWithFormat:@"https://%@", urlString];
+        
+        DDLogDebug(@"WebXR-iOS viewer opened with URL: %@", urlString);
+        
+        [[NSUserDefaults standardUserDefaults] setObject:urlString forKey:REQUESTED_URL_KEY];
+
+        return YES;
+    }
+    return NO;
+}
+
 @end
