2e9b4295b175fa4c975b4f5c9a79329caf5ac8d8
Roberto Garrido <robertogarridomartin@gmail.com>
2017-12-01 15:41:17 +0100

    Fixed the image name for the ARKit disabled tracking state

--- XRViewer/Overlay/OverlayViewController.m
+++ XRViewer/Overlay/OverlayViewController.m
@@ -611,7 +611,7 @@
                     completion(finish);
                 }];
 
-                [[self trackingStateButton] setImage:[UIImage imageNamed:@"disabled"] forState:UIControlStateNormal];
+                [[self trackingStateButton] setImage:[UIImage imageNamed:@"ARKitNotAvailable"] forState:UIControlStateNormal];
             }
 
             return;
