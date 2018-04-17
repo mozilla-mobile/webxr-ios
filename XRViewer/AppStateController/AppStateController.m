4ae6c619114e115d9115b4880687ac25ce39cff9
Roberto Garrido <robertogarridomartin@gmail.com>
2018-04-17 19:27:16 +0200

    Send the native time before sending the AR data.

--- XRViewer/AppStateController/AppStateController.m
+++ XRViewer/AppStateController/AppStateController.m
@@ -139,7 +139,7 @@ typedef NS_ENUM(NSUInteger, ExclusiveStateType)
 }
 
 - (BOOL)shouldSendNativeTime {
-    return [[self state] numberOfTimesSendNativeTimeWasCalled] < 9;
+    return [[self state] numberOfTimesSendNativeTimeWasCalled] < 10;
 }
 
 - (void)invertMic
