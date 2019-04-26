230a4074bd48178a4f459ae4040d277738614fb0
Blair MacIntyre <bmacintyre@mozilla.com>
2019-04-26 13:04:18 -0400

    change where the GeomArrays flag is

--- XRViewer/WebController/WebController.swift
+++ XRViewer/WebController/WebController.swift
@@ -304,7 +304,7 @@ class WebController: NSObject, WKUIDelegate, WKNavigationDelegate, WKScriptMessa
                     DDLogDebug("Init AR Success")
                     guard let arRequestOption = messageBody[WEB_AR_REQUEST_OPTION] as? [String: Any] else { return }
                     guard let arUIOption = arRequestOption[WEB_AR_UI_OPTION] as? [String: Any] else { return }
-                    if messageBody[WEB_AR_GEOMETRY_ARRAYS] as? Bool ?? false {
+                    if arRequestOption[WEB_AR_GEOMETRY_ARRAYS] as? Bool ?? false {
                         UserDefaults.standard.set(true, forKey: Constant.geometryArraysKey())
                     } else {
                         UserDefaults.standard.set(false, forKey: Constant.geometryArraysKey())
