125da610c7079c65fbb4f547780a686f9762f7fe
Tony Morales <atmorales@gmail.com>
2019-05-08 16:23:06 -0500

    Fixes #142

--- XRViewer/RequestPermissionsController/RequestXRPermissionsViewController.swift
+++ XRViewer/RequestPermissionsController/RequestXRPermissionsViewController.swift
@@ -19,6 +19,6 @@ class RequestXRPermissionsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.rowHeight = 44
     }
 }
