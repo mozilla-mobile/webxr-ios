564b9f55cb6236ccfe3cba098e953f5f762cda10
Tony Morales <atmorales@gmail.com>
2019-07-17 17:32:42 -0500

    Add Extension: UIButton addAction closure

--- XRViewer/Utilities/Utils.swift
+++ XRViewer/Utilities/Utils.swift
@@ -125,3 +125,23 @@ extension Dictionary where Value: Equatable {
 func appDelegate() -> AppDelegate {
     return UIApplication.shared.delegate as! AppDelegate
 }
+
+class ClosureSleeve {
+    let closure: ()->()
+    
+    init (_ closure: @escaping ()->()) {
+        self.closure = closure
+    }
+    
+    @objc func invoke () {
+        closure()
+    }
+}
+
+extension UIControl {
+    func addAction(for controlEvents: UIControl.Event, _ closure: @escaping ()->()) {
+        let sleeve = ClosureSleeve(closure)
+        addTarget(sleeve, action: #selector(ClosureSleeve.invoke), for: controlEvents)
+        objc_setAssociatedObject(self, "[\(arc4random())]", sleeve, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+    }
+}
